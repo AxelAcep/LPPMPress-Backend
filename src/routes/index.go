6912466877ef11d6c4f273387bb75e9b1601e982
AxelAcep <axelsebayang456@gmail.com>
2026-02-23 11:04:07 +0700

    Backend Register Bug and Cors add

--- src/routes/index.go
+++ src/routes/index.go
@@ -2,10 +2,17 @@ package routes
 
 import (
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func Init() *echo.Echo {
 	e := echo.New()
+
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+        AllowOrigins: []string{"http://localhost:3000"},
+        AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization},
+        AllowMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    }))
 
 	GuestRoutes(e)
 	UserRoutes(e)
